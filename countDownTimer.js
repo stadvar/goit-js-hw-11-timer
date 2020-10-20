0c3f362c192154519bd9e058c57dd289b6f87131
stadvar <stadvar@gmail.com>
2020-10-20 12:10:46 +0300

    ready

---
+++ countDownTimer.js
@@ -0,0 +1,44 @@
+export default class CountdownTimer {
+  constructor({ selector, targetDate }) {
+    this.selector = selector;
+    this.targetDate = targetDate;
+    this.wrapper = document.querySelector(this.selector);
+    this.days = this.wrapper.querySelector('[data-value="days"]');
+    this.hours = this.wrapper.querySelector('[data-value="hours"]');
+    this.mins = this.wrapper.querySelector('[data-value="mins"]');
+    this.secs = this.wrapper.querySelector('[data-value="secs"]');
+    this.timerId = null;
+    this.isTimerActiv = false;
+  }
+  start() {
+    if (this.isTimerActiv) return;
+    this.isTimerActiv = true;
+    this.timerId = setInterval(() => {
+      const time = this.targetDate - Date.now();
+      this.updateInterface(time);
+    }, 1000);
+  }
+  stop() {
+    this.isTimerActiv = false;
+    clearInterval(this.timerId);
+    this.updateInterface(0);
+  }
+  updateInterface(time) {
+    const { days, hours, mins, secs } = this.getTimeComponents(time);
+    this.days.textContent = days;
+    this.hours.textContent = hours;
+    this.mins.textContent = mins;
+    this.secs.textContent = secs;
+  }
+  getTimeComponents(time) {
+    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+
+    return { days, hours, mins, secs };
+  }
+  pad(value) {
+    return String(value).padStart(2, '0');
+  }
+}
---
+++ script.js
@@ -0,0 +1,33 @@
+import CountdownTimer from './countDownTimer.js';
+const timer = new CountdownTimer({
+  selector: '#timer-1',
+  targetDate: new Date('Nov 1, 2020'),
+});
+//--
+document.querySelector('[data-value="start"]').addEventListener('click', timer.start.bind(timer));
+document.querySelector('[data-value="stop"]').addEventListener('click', timer.stop.bind(timer));
+
+/*
+ * Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество
+ * миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
+ */
+// const days = Math.floor(time / (1000 * 60 * 60 * 24));
+
+/*
+ * Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
+ * остатка % и делим его на количество миллисекунд в одном часе
+ * (1000 * 60 * 60 = миллисекунды * минуты * секунды)
+ */
+// const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+
+/*
+ * Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
+ * миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
+ */
+// const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+
+/*
+ * Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
+ * миллисекунд в одной секунде (1000)
+ */
+// const secs = Math.floor((time % (1000 * 60)) / 1000);
