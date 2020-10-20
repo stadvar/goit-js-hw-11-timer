d621bf1e3190ee955ea480dffad93a8e8aef7eec
stadvar <stadvar@gmail.com>
2020-10-20 17:38:46 +0300

    added header

--- countDownTimer.js
+++ countDownTimer.js
@@ -31,7 +31,7 @@ export default class CountdownTimer {
     this.secs.textContent = secs;
   }
   getTimeComponents(time) {
-    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const days = Math.floor(time / (1000 * 60 * 60 * 24));
     const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
     const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
     const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
